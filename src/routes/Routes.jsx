44d16add7104fc02f2647479c111b0025613c339
Mohin Rana <ebrahimit49@gmail.com>
2024-01-22 22:01:12 +0600

    Rental House website client side project setup

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,30 @@
+
+
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+
+const myRoutes = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                path:"/",
+                element: <Home />
+            },
+            {
+                path:"/login",
+                element: <Login />
+            },
+            {
+                path:"/register",
+                element: <Register />
+            },
+        ]
+    }
+])
+
+export default myRoutes;
